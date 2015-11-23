5ae88668cbc67258b1f693957f71e67ee0892462
natete <natete981@gmail.com>
2015-11-24 00:42:03 +0100

    CU_4 basic search

---
+++ src/ejb/TripFacadeBean.java
@@ -0,0 +1,60 @@
+package ejb;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.ParameterExpression;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
+import jpa.TripJPA;
+
+@Stateless
+public class TripFacadeBean implements TripFacadeRemote {
+	
+	private static final String BLANK = "";
+
+	// Persistence Unit Context
+	@PersistenceContext(unitName = "CarSharing")
+	private EntityManager entman;
+
+	/**
+	 * @see TripFacadeRemote#findTrips(String, Date, String)
+	 */
+	@Override
+	public List<TripJPA> findTrips(String departureCity, Date departureDate, String arrivalCity) {
+		List<TripJPA> result;
+		
+		CriteriaBuilder cb = entman.getCriteriaBuilder();
+		
+		CriteriaQuery<TripJPA> q = cb.createQuery(TripJPA.class);
+		Root<TripJPA> c = q.from(TripJPA.class);
+		List<Predicate> predicates = new ArrayList<>();
+		
+		if(departureCity != null && !BLANK.equals(departureCity)){
+			Predicate pred = cb.like(cb.upper(c.get("departureCity")), departureCity.toUpperCase() + "%");
+			predicates.add(pred);
+		}
+		if(departureDate != null){
+			Predicate pred = cb.equal(c.get("departureCity"), departureDate);
+			predicates.add(pred);
+		}
+		if(arrivalCity != null && !BLANK.equals(arrivalCity)){
+			Predicate pred = cb.like(cb.upper(c.get("arrivalCity")), arrivalCity.toUpperCase() + "%");
+			predicates.add(pred);
+		}
+		
+		q.where(cb.and(predicates.toArray(new Predicate[0])));
+		
+		final TypedQuery<TripJPA> query = entman.createQuery(q);
+		
+		return query.getResultList();
+	}
+}
---
+++ src/jpa/TripJPA.java
@@ -0,0 +1,113 @@
+package jpa;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="trip")
+public class TripJPA implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Integer id;
+	private String description;
+	private String departureCity;
+	private String fromPlace;
+	private Date departureDate;
+	private Date departureTime;
+	private String arrivalCity;
+	private String toPlace;
+	private Integer availableSeats;
+	private float price;
+	private float driverRating;
+	
+	public TripJPA() {
+		super();
+	}
+	
+	public TripJPA(String description, String departureCity, String fromPlace, Date departureDate, Date departureTime,
+			String arrivalCity, String toPlace, Integer availableSeats, float price) {
+		super();
+		this.description = description;
+		this.departureCity = departureCity;
+		this.fromPlace = fromPlace;
+		this.departureDate = departureDate;
+		this.departureTime = departureTime;
+		this.arrivalCity = arrivalCity;
+		this.toPlace = toPlace;
+		this.availableSeats = availableSeats;
+		this.price = price;
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getDepartureCity() {
+		return departureCity;
+	}
+	public void setDepartureCity(String departureCity) {
+		this.departureCity = departureCity;
+	}
+	public String getFromPlace() {
+		return fromPlace;
+	}
+	public void setFromPlace(String fromPlace) {
+		this.fromPlace = fromPlace;
+	}
+	public Date getDepartureDate() {
+		return departureDate;
+	}
+	public void setDepartureDate(Date departureDate) {
+		this.departureDate = departureDate;
+	}
+	public Date getDepartureTime() {
+		return departureTime;
+	}
+	public void setDepartureTime(Date departureTime) {
+		this.departureTime = departureTime;
+	}
+	public String getArrivalCity() {
+		return arrivalCity;
+	}
+	public void setArrivalCity(String arrivalCity) {
+		this.arrivalCity = arrivalCity;
+	}
+	public String getToPlace() {
+		return toPlace;
+	}
+	public void setToPlace(String toPlace) {
+		this.toPlace = toPlace;
+	}
+	public Integer getAvailableSeats() {
+		return availableSeats;
+	}
+	public void setAvailableSeats(Integer availableSeats) {
+		this.availableSeats = availableSeats;
+	}
+	public float getPrice() {
+		return price;
+	}
+	public void setPrice(float price) {
+		this.price = price;
+	}
+	public float getDriverRating() {
+		return driverRating;
+	}
+	public void setDriverRating(float driverRating) {
+		this.driverRating = driverRating;
+	}
+}

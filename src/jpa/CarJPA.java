03742fa9af7d761359c40892cee4957e9932f5d2
natete <natete981@gmail.com>
2015-11-29 20:09:10 +0100

    Basic entities

--- src/jpa/CarJPA.java
+++ src/jpa/CarJPA.java
@@ -5,7 +5,7 @@ import java.io.Serializable;
 import javax.persistence.*;
 
 /**
- * JPA Class PetJPA
+ * JPA Class CarJPA
  */
 @Entity
 @Table(name="car")
@@ -13,12 +13,13 @@ public class CarJPA implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
-	private String nif;
 	private String carRegistrationId;
 	private String brand;
 	private String model;
 	private String color;
-
+//	private Collection<TripJPA> trips;
+	private DriverJPA driver;
+	
 	/**
 	 * Class constructor methods
 	 */
@@ -26,26 +27,17 @@ public class CarJPA implements Serializable {
 		super();
 	}
 
-	public CarJPA(String nif, String carRegistrationId, String brand, String model, String color) {
-		this.nif = nif;
+	public CarJPA(String carRegistrationId, String brand, String model, String color) {
 		this.carRegistrationId = carRegistrationId;
 		this.brand = brand;
 		this.model = model;
 		this.color = color;
-
 	}
 
 	/**
 	 * Methods get/set the fields of database
 	 */
-	public String getNif() {
-		return nif;
-	}
-
-	public void setNif(String nif) {
-		this.nif = nif;
-	}
-
+	
 	@Id
 	public String getCarRegistrationId() {
 		return carRegistrationId;
@@ -77,5 +69,26 @@ public class CarJPA implements Serializable {
 
 	public void setColor(String color) {
 		this.color = color;
+	}
+	
+	// persistent relationships
+	/*@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER )
+	@JoinColumn(name = "car")
+	public Collection<TripJPA> getTripsByCar() {
+		return trips;
+	}
+	
+	public void setTripsByCar(Collection<TripJPA> trips) {
+		this.trips = trips;
+	}
+*/
+	@ManyToOne
+	@JoinColumn (name="driver")
+	public DriverJPA getDriver() {
+		return driver;
+	}
+	
+	public void setDriver(DriverJPA driver) {
+		this.driver = driver;
 	}
 }
---
+++ src/jpa/DriverJPA.java
@@ -0,0 +1,142 @@
+package jpa;
+
+import java.io.Serializable;
+import java.util.Collection;
+
+import javax.persistence.*;
+
+/**
+ * JPA Class DriverJPA
+ */
+@Entity
+@Table(name="driver")
+public class DriverJPA implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private String nif;
+	private String name;
+	private String surname;
+	private String phone;
+	private String password;
+	private String email;
+	private Collection<CarJPA> cars;
+//	private Collection<TripJPA> trips;
+//	private Collection<DriverCommentsJPA> driverComments;
+//	private Collection<MessageJPA> messages;
+	
+	/**
+	 * Class constructor methods
+	 */
+	public DriverJPA() {
+		super();
+	}
+
+	public DriverJPA(String nif, String name, String surname, String phone, String password, String email) {
+		this.nif = nif;
+		this.name = name;
+		this.surname = surname;
+		this.phone = phone;
+		this.password = password;
+		this.email = email;
+
+	}
+
+	/**
+	 * Methods get/set the fields of database
+	 */
+	
+	@Id
+	public String getNif() {
+		return nif;
+	}
+
+	public void setNif(String nif) {
+		this.nif = nif;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	
+	@OneToMany(mappedBy = "driver")
+	public Collection<CarJPA> getCars() {
+		return cars;
+	}
+
+	public void setCars(Collection<CarJPA> cars) {
+		this.cars = cars;
+	}
+
+	// persistent relationships
+	/*@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER )
+	@JoinColumn(name = "driver")
+	public Collection<TripJPA> getTripsByDriver() {
+		return trips;
+	}
+	
+	public void setTripsByDriver(Collection<TripJPA> trips) {
+		this.trips = trips;
+	}
+
+	/*@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER )
+	@JoinColumn(name = "driver")
+	public Collection<DriverCommentJPA> getDriverCommentsByDriver() {
+		return driverComments;
+	}
+	
+	public void setDriverCommentsByDriver(Collection<DriverCommentsJPA> driverComments) {
+		this.driverComments = driverComments;
+	}
+	
+	// persistent relationships
+	/*@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER )
+	@JoinColumn(name = "driver")
+	public Collection<messageJPA> getMessagesByDriver() {
+		return messages;
+	}
+	
+	public void setMessagesByDriver(Collection<MessageJPA> messages) {
+		this.messages = messages;
+	}*/
+	
+	
+
+}

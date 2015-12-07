a40aaa3d55c39244d8d331b2f3256b317b8d2e1d
natete <natete981@gmail.com>
2015-12-07 13:31:05 +0100

    Translatation to English

--- src/managedbean/FindTripsMBean.java
+++ src/managedbean/FindTripsMBean.java
@@ -23,51 +23,65 @@ public class FindTripsMBean implements Serializable {
 
 	@EJB
 	private TripFacadeRemote tripFacadeRemote;
-	
+
 	private String departureCity;
 	private Date departureDate;
 	private String arrivalCity;
 	private List<TripJPA> trips;
-	
-	private final String emptyListMessage = "<i class='fa fa-times'></i> La seva cerca no ha produït resultats...";
-	
+
+	private final String emptyListMessage = "<i class='fa fa-times'></i> Sorry we have no trips matching your search criteria right now...";
+
+	private String searchMessage = "";
+
 	public FindTripsMBean() {
 		super();
+
 	}
 
 	public void findTrips() throws NamingException {
 		Properties props = System.getProperties();
 		Context ctx = new InitialContext(props);
-		tripFacadeRemote = (TripFacadeRemote) ctx.lookup("java:app/CAT-PDP-GRUP6.jar/TripFacadeBean!ejb.TripFacadeRemote");
+		tripFacadeRemote = (TripFacadeRemote) ctx
+				.lookup("java:app/CAT-PDP-GRUP6.jar/TripFacadeBean!ejb.TripFacadeRemote");
 		trips = tripFacadeRemote.findTrips(departureCity, departureDate, arrivalCity);
+		if (trips == null || trips.isEmpty()) {
+			searchMessage = emptyListMessage;
+		}
 	}
 
 	public String getDepartureCity() {
 		return departureCity;
 	}
+
 	public void setDepartureCity(String departureCity) {
 		this.departureCity = departureCity;
 	}
+
 	public Date getDepartureDate() {
 		return departureDate;
 	}
+
 	public void setDepartureDate(Date departureDate) {
 		this.departureDate = departureDate;
 	}
+
 	public String getArrivalCity() {
 		return arrivalCity;
 	}
+
 	public void setArrivalCity(String arrivalCity) {
 		this.arrivalCity = arrivalCity;
 	}
+
 	public List<TripJPA> getTrips() {
 		return trips;
 	}
+
 	public void setTrips(List<TripJPA> trips) {
 		this.trips = trips;
 	}
 
-	public String getEmptyListMessage() {
-		return emptyListMessage;
+	public String getsearchMessage() {
+		return searchMessage;
 	}
 }

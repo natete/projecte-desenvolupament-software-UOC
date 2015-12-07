221598e4ba444b350a773deb77b065817f75b69f
natete <natete981@gmail.com>
2015-12-07 18:23:44 +0100

    Filters requests according permission

---
+++ src/config/AuthorizationFilter.java
@@ -0,0 +1,73 @@
+package config;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import managedbean.SessionBean;
+
+@WebFilter(filterName = "AuthFilter", urlPatterns = { "*.xhtml" })
+public class AuthorizationFilter implements Filter {
+
+	private static final String LOGIN_PAGE = "/CAT-PDP-GRUP6/pages/public/login.xhtml";
+	private static final String DRIVER_PATH = "/driver/";
+	private static final String PASSENGER_PATH = "/passenger/";
+
+	public AuthorizationFilter() {
+		super();
+	}
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+
+		try {
+			HttpServletRequest servletRequest = (HttpServletRequest) request;
+			HttpServletResponse servletResponse = (HttpServletResponse) response;
+			HttpSession session = servletRequest.getSession(false);
+
+			String reqURI = servletRequest.getRequestURI();
+			if (reqURI.indexOf(DRIVER_PATH) >= 0) {
+				boolean isDriver = session != null ? (boolean) session.getAttribute(SessionBean.DRIVER_ROLE) : false;
+				if (isDriver) {
+					chain.doFilter(request, response);
+				} else {
+					servletResponse.sendRedirect(LOGIN_PAGE);
+				}
+			} else if (reqURI.indexOf(PASSENGER_PATH) >= 0) {
+				boolean isPassenger = session != null ? (boolean) session.getAttribute(SessionBean.PASSENGER_ROLE)
+						: false;
+				if (isPassenger) {
+					chain.doFilter(request, response);
+				} else {
+					servletResponse.sendRedirect(LOGIN_PAGE);
+				}
+			} else {
+				chain.doFilter(request, response);
+			}
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+
+	}
+
+	@Override
+	public void destroy() {
+
+	}
+
+}

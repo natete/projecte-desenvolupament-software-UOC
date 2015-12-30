e40a08599a484951204c47e7cb6de0223bf41517
jparedesr <jparedesr@uoc.edu>
2015-12-30 14:16:05 +0100

    mas javadoc

--- src/ejb/UserFacadeRemote.java
+++ src/ejb/UserFacadeRemote.java
@@ -22,28 +22,96 @@ public interface UserFacadeRemote {
 
 	/**
 	 * Add a car in a driver
-	 * @param carRegistrationId id of the car
-	 * @param brand brand of the car
-	 * @param model model of the car
-	 * @param color color of the car
+	 * 
+	 * @param carRegistrationId
+	 *            id of the car
+	 * @param brand
+	 *            brand of the car
+	 * @param model
+	 *            model of the car
+	 * @param color
+	 *            color of the car
 	 */
 	void addCar(String nif, String carRegistrationId, String brand, String model, String color);
 
 	/**
 	 * List all car of a driver
-	 * @param nif nif of the driver
+	 * 
+	 * @param nif
+	 *            nif of the driver
 	 * @return List<{{@link TripJPA}> list of the cars
 	 */
 	List<CarJPA> listAllCars(String nif);
 
+	/**
+	 * Check for trips associated with a car
+	 * 
+	 * @param carRegistrationId
+	 *            id of the car
+	 * @return boolean
+	 */
 	boolean carHasTrips(String carRegistrationId);
 
+	/**
+	 * Delete a car of a driver
+	 * 
+	 * @param carRegistrationId
+	 *            id of the car
+	 */
 	void deleteCar(String carRegistrationId);
 
+	/**
+	 * Registers a driver
+	 * 
+	 * @param nif
+	 *            nif of the driver
+	 * @param name
+	 *            name of the driver
+	 * @param surname
+	 *            surname of the driver
+	 * @param phone
+	 *            phone of the driver
+	 * @param nif
+	 *            password password of the driver
+	 * @param nif
+	 *            email email of the driver
+	 */
 	void registerDriver(String nif, String name, String surname, String phone, String password, String email);
 
+	/**
+	 * Registers a passenger
+	 * 
+	 * @param nif
+	 *            nif of the passenger
+	 * @param name
+	 *            name of the passenger
+	 * @param surname
+	 *            surname of the passenger
+	 * @param phone
+	 *            phone of the passenger
+	 * @param nif
+	 *            password password of the passenger
+	 * @param nif
+	 *            email email of the passenger
+	 */
 	void registerPassenger(String nif, String name, String surname, String phone, String password, String email);
 
+	/**
+	 * Registers a passenger
+	 * 
+	 * @param nif
+	 *            nif of the passenger
+	 * @param name
+	 *            name of the passenger
+	 * @param surname
+	 *            surname of the passenger
+	 * @param phone
+	 *            phone of the passenger
+	 * @param nif
+	 *            password password of the passenger
+	 * @param nif
+	 *            email email of the passenger
+	 */
 	UserDTO login(String email, String password);
 
 	UserJPA findUser(String nif);
